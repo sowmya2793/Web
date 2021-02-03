b4cf81337ed65bbc75999566e6afb7991bfe3668
Sowmya kini <sowmyaskini@gmail.com>
2021-02-03 14:21:00 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,19 @@
+
+Currency converter which converts values between Euro, US Dollar and Japanese Yen.
+- Markup is not very elaborate and applied best practices.
+- Styling No use of any post- or pre-processors and without any styling libraries like Bootstrap or Foundation.
+- JavaScript talks to the API endpoint for the currency conversion (using [fixer.io](http://fixer.io/) 
+- Unpaid version of API endpoit used currently
+- Ajax calls with [`axios`](https://github.com/axios/axios) which is included at the bottom of the `index.html` file)
+- No use of any other libraries or frameworks like jQuery, Angular, React, etc.
+- Improved UI and ensured the interface works for multiple device types.
+
+
+
+
+- Showed  history of conversion inputs.
+- Kept the history persistent on browser reload.
+
+To be done
+-(progress: transpiling or vendor-prefixing things)
+- use of ES6 features and new CSS technologies 
---
+++ scripts.js
@@ -0,0 +1,49 @@
+// JavaScript goes here.
+var EUR = document.getElementById('EUR');
+var USD = document.getElementById('USD');
+var JPY = document.getElementById('JPY');
+var historyTable = document.getElementById('searchHistory');
+var resultRates;
+var historyStored = localStorage.getItem('conversionHistory');
+if(historyStored) { historyTable.innerHTML = historyStored; } else { historyStored=""}
+//API call for currency conversion
+axios.get('http://data.fixer.io/api/latest?access_key=84b95c8bda042263b8948863ac570c63&format=1').then(function(response){
+    resultRates = response.data.rates;
+    EUR.value = resultRates.EUR;
+    USD.value = resultRates.USD;
+    JPY.value = resultRates.JPY;
+})
+.catch(function(error){
+       console.log(error);
+});
+
+EUR.addEventListener('input',function(){
+    if(isNaN(parseFloat(EUR.value))){ EUR.value = 0; } 
+    currencyConverter(parseFloat(EUR.value),this.id,"all")
+});
+USD.addEventListener('input',function(){ 
+    if(isNaN(parseFloat(USD.value))){ USD.value = 0; } 
+    currencyConverter(parseFloat(USD.value),this.id,"JPY");
+});
+JPY.addEventListener('input',function(){
+    if(isNaN(parseFloat(JPY.value))){ JPY.value = 0; }  
+    currencyConverter(parseFloat(JPY.value),this.id,"USD");
+});
+
+var currencyConverter = (currencyValue,currency,convertValue) => {
+    if(currency == "EUR"){
+        USD.value = (parseFloat(EUR.value)*resultRates.USD).toFixed(4);
+        JPY.value = (parseFloat(EUR.value)*resultRates.JPY).toFixed(4);
+    }
+    else if(currency == "USD"){
+        EUR.value= parseFloat(currencyValue/resultRates[currency]).toFixed(4);
+        JPY.value= parseFloat(EUR.value*resultRates[convertValue]).toFixed(4);
+    }else{
+        EUR.value= parseFloat(currencyValue/resultRates[currency]).toFixed(4);
+        USD.value= parseFloat(EUR.value*resultRates[convertValue]).toFixed(4); 
+    }
+    historyStored +="<tr><td>" + parseFloat(EUR.value) + "</td><td>" + parseFloat(USD.value) + "</td><td>" + parseFloat(JPY.value) +"</td></tr>";
+    historyTable.innerHTML = historyStored;
+	//Browser reload persistent storage
+	localStorage.setItem('conversionHistory',historyTable.innerHTML);
+}
